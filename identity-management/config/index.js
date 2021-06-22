7b47e5270b84e0c9a45b76063ad1e15349e0ba71
LAPTOP-LCNRS1QQ\Ben Bolthouse <benbolthouse@gmail.com>
2021-06-22 07:55:54 -0700

    Add Express details to environment vars

--- identity-management/config/index.js
+++ identity-management/config/index.js
@@ -9,4 +9,9 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     user: process.env.DATABASE_USER,
   },
+  express: {
+    host: process.env.EXPRESS_HOST,
+    port: process.env.EXPRESS_PORT,
+  },
+  isProduction: process.env.ENVIORNMENT === 'production',
 };
