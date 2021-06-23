ae24ab4db4b73f04b25c8051ee683b735194cd8a
LAPTOP-LCNRS1QQ\Ben Bolthouse <benbolthouse@gmail.com>
2021-06-22 17:05:26 -0700

    Create a tests setup fixture

---
+++ identity-management/tests/_setup.js
@@ -0,0 +1,67 @@
+const { DataTypes } = require('sequelize');
+
+const { logger } = require('../config/logger');
+const { sequelize: sequelizeTest } = require('../models');
+const config = require('../config');
+const sequelizeAdmin = require('./_sequelize');
+const usersTableMigration = require('../migrations/00001-create-users-table');
+const rolesTableMigration = require('../migrations/00002-create-roles-table');
+
+let setupHasRun = false;
+
+async function setup() {
+  if (setupHasRun) return logger.info('Setup already complete, skipping...');
+
+  const adminQueryInterface = sequelizeAdmin.getQueryInterface();
+  const testQueryInterface = sequelizeTest.getQueryInterface();
+  const databaseName = config.database.name;
+
+  // The following will initially try to drop the database if it exists.
+  try {
+    logger.info('Removing existing database...');
+
+    await adminQueryInterface.dropDatabase(databaseName);
+
+    logger.info('Database removed.');
+  }
+  catch (e) {
+    logger.warn(e);
+  }
+
+  // Then the second step to the database setup is creating a database,
+  // requiring two separate try/catches. The above try/catch will catch if the
+  // database does not already exist.
+  try {
+    logger.info('Creating test database...');
+
+    await adminQueryInterface.createDatabase(databaseName);
+
+    logger.info('Database created.');
+  }
+  catch (e) {
+    logger.error(e);
+  }
+
+  // The following runs a list of migrations. Note that while migrations
+  // upgrades run here, downgrades are not necessary due to deletion of the test
+  // database on teardown.
+  try {
+    logger.info('Running migrations...');
+
+    await usersTableMigration.up(testQueryInterface, DataTypes);
+    await rolesTableMigration.up(testQueryInterface, DataTypes);
+
+    logger.info('Migrations complete.');
+  }
+  catch (e) {
+    logger.error(e);
+  }
+
+  // This final step ensures that we don't accidentally try to create the
+  // database twice or run migrations twice.
+  setupHasRun = true;
+
+  return testQueryInterface;
+}
+
+module.exports = setup;
